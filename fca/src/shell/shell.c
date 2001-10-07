e91ca8ea0492d2cb097a8df0b7ccf19d63fc5e34
kik <kik@7c08a058-91f0-4b81-8c51-e87c80606beb>
2001-10-07 08:09:58 +0000

    DQ3が時々panicする。特に画面が切り替わるとき。

--- fca/src/shell/shell.c
+++ fca/src/shell/shell.c
@@ -48,9 +48,14 @@ panic(int op, int pc, unsigned char *sp, void *p)
 {
   int i;
 
+  _ioreg[0x104] = 0;
+  _ioreg[0xBA / 2] = 0;
+  _ioreg[0xC6 / 2] = 0;
   init_text_console(0, 2, 0x1C);
 
   _ioreg[0] = 0x0140;
+  _ioreg[8] = 0;
+  _ioreg[9] = 0;
   _ioreg[5] = 0x0704;
   _ioreg[0x102] = 0x4004;
   for (i = 0; i < 32 * 32; i++)
@@ -60,8 +65,46 @@ panic(int op, int pc, unsigned char *sp, void *p)
   printf("OP: %x\n", op);
   printf("PC: %x(%x)\n", pc, p);
   printf("stack dump:\n");
-  for (i = 0; i < 8; i++, sp += 4)
-    printf("%x %x %x %x\n", sp[0], sp[1], sp[2], sp[3]);
+  
+  sp -= 32;
+  for (i = 0; i < 4; i++, sp += 8)
+    printf("%x %x %x %x %x %x %x %x\n", sp[0], sp[1], sp[2], sp[3],
+	   sp[4], sp[5], sp[6], sp[7]);
+
+  printf("---SP---\n");
+
+  for (i = 0; i < 4; i++, sp += 8)
+    printf("%x %x %x %x %x %x %x %x\n", sp[0], sp[1], sp[2], sp[3],
+	   sp[4], sp[5], sp[6], sp[7]);
+
+  while (1)
+    ;
+}
+
+void
+panic_from_int()
+{
+  int i;
+  int *p = (int *)0x03000000;
+
+  _ioreg[0xBA / 2] = 0;
+  _ioreg[0xC6 / 2] = 0;
+
+  init_text_console(0, 2, 0x1C);
+
+  _ioreg[0] = 0x0140;
+  _ioreg[8] = 0;
+  _ioreg[9] = 0;
+  //_ioreg[4] = 0x1C08;
+  //_ioreg[0x102] = 0x4004;
+#if 0
+  for (i = 0; i < 32 * 32; i++)
+    _vram[0x2000 + i] = i;
+#endif
+  //printf("\n\n\n\n\n");
+  printf("PANIC from int!!!\n");
+  for (i = 0; i < 9; i++, p += 2)
+    printf("%x %x\n", p[0], p[1]);
 
   while (1)
     ;

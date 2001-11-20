79a41ae61630d85bbf984ca616ea4d59fe349a34
kik <kik@7c08a058-91f0-4b81-8c51-e87c80606beb>
2001-11-20 17:30:06 +0000

    vram.hを移動。

--- fca/src/include/text.h
+++ fca/src/include/text.h
@@ -1,14 +1,46 @@
-#ifndef TEXT_H
-#define TEXT_H
+#ifndef VRAM_H
+#define VRAM_H
 
-int init_text_console(int bkg, int tile, int map);
+#define VRAM_BASE_ADDR  0x06000000
 
-void scroll();
+#ifndef __ASSEMBLY__
 
-int putcharxy(char *c, int x, int y);
-int putchar(char *c);
-void putsxy(char *s, int x, int y);
-void puts(char *s);
-void printf(char *fmt, ...);
+#define VRAM_BASE       ((short *)VRAM_BASE_ADDR)
+
+#define VRAM_FONT_BASE  ((short *)(VRAM_BASE_ADDR + 0x8000))
+#define VRAM_TEXT_BASE  ((short *)(VRAM_BASE_ADDR + 0xF800))
+
+#endif
+
+#define FONT_TOFU_TR  0xF100 // ∆©Ã¿§ ∆¶…Â
+#define FONT_TOFU_B   0xF101 // πı§§∆¶…Â
+#define FONT_TOFU_W   0xF102 // «Ú§§∆¶…Â
+
+#define FONT_LINE_U   0xF103
+#define FONT_LINE_D   0xF903
+#define FONT_LINE_L   0xF104
+#define FONT_LINE_R   0xF504
+
+#define FONT_LINE_UL  0xF105
+#define FONT_LINE_UR  0xF505
+#define FONT_LINE_DL  0xF905
+#define FONT_LINE_DR  0xFD05
+
+#define FONT_CUR_U    0xF107
+#define FONT_CUR_D    0xF907
+#define FONT_CUR_L    0xF106
+#define FONT_CUR_R    0xF506
+
+#ifndef __ASSEMBLY__
+
+void init_font();
+void refresh_screen();
+void putcxy(short c, int x, int y);
+void putsxy(unsigned char *s, int x, int y);
+void printfxy(int x, int y, unsigned char *s, ...);
+void draw_rect(int x, int y, int w, int h);
+void clear_screen();
+
+#endif
 
 #endif
